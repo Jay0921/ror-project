f22ea80b7c8944675d7264392ba86312a3d4f978
Jay <purporz@gmail.com>
2020-01-15 21:04:45 +0800

    Add slim-rails

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ class ActionDispatch::Routing::Mapper
 end
 
 Rails.application.routes.draw do
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
