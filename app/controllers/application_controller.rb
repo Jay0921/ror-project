07313b53a647a3631172f0737d7ae92850eabc6e
Jay <purporz@gmail.com>
2020-01-19 13:24:58 +0800

    Add paper_trail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_paper_trail_whodunnit
+
+  def user_for_paper_trail
+    current_user&.email || 'System'
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_back(fallback_location: root_path, notice: exception.message)
   end
