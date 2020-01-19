54a78728cafeedcd42c3c0a0f73020b9a6369895
Jay <purporz@gmail.com>
2020-01-19 10:23:54 +0800

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ class ActionDispatch::Routing::Mapper
 end
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :pugs
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
