a80463df7072730727f6e0f0749abce1fe8e648d
Jay <purporz@gmail.com>
2020-01-21 21:09:40 +0800

    Devise skip_confirmation for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,6 +33,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_save :skip_confirmation!
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
