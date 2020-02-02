ef7a385fa0fdbef7169f35ecd8d30f64cabb9f4b
Jay <purporz@gmail.com>
2020-02-02 16:02:55 +0800

    Fix AddRoleToUsers migration issue

--- db/migrate/20200119023020_add_role_to_users.rb
+++ db/migrate/20200119023020_add_role_to_users.rb
@@ -1,5 +1,10 @@
 class AddRoleToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :role, :integer, default: 0
+  def up
+    add_column :users, :role, :integer
+    change_column_default :users, :role, 0
+  end
+
+  def down
+    remove_column :users, :role
   end
 end
