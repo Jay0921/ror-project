35648399a1b21668f5c25cb71defd65dfe009086
Jay <purporz@gmail.com>
2020-03-04 20:22:41 +0800

    User listing page
    - Add user listing page
    - Add destroy and search function

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,20 @@
+module Admin
+  class UsersController < BaseController
+    before_action :set_user, only: %i[destroy]
+
+    def index
+      @q = User.ransack(params[:q])
+      @users = @q.result.order("id ASC").page(params[:page])
+    end
+
+    def destroy
+      @user.destroy
+      redirect_to admin_users_path, notice: "User was successfully deleted."
+    end
+
+    private
+    def set_user
+      @user = User.find(params[:id])
+    end
+  end
+end
--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -1,4 +1,6 @@
 namespace :admin do
   root to: "admin#index"
   get '/second', to: "admin#second"
+
+  resources :users, only: %i[index destroy]
 end
