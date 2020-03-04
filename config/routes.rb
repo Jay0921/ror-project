6d1260830b24ee606b7882862d98963dcd595840
Jay <purporz@gmail.com>
2020-03-04 21:35:39 +0800

    Sidekiq
    - Add sidekiq gem
    - Update rack version to be greater than 2.1.1
    - Add sidekiq web to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require "sidekiq/web"
+
 class ActionDispatch::Routing::Mapper
   def draw(routes_name)
     instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
@@ -17,6 +19,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   mount GrapeSwaggerRails::Engine => '/swagger'
+  mount Sidekiq::Web => "/sidekiq"
 
   constraints CanAccessFlipperUI do
     mount Flipper::UI.app(Flipper) => '/flipper'
