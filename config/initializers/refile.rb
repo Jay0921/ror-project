a5b1b5d414d54b295753ba9ea1f5e0208bf11f9d
Jay <purporz@gmail.com>
2020-03-14 21:59:07 +0800

    Refile S3
    - Enable S3 bucket

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,11 +1,11 @@
 require "refile/s3"
 
-# aws = {
-#   access_key_id: "XXXX",
-#   secret_access_key: "XXXX",
-#   region: "ap-southeast-1",
-#   bucket: "s3-bucket"
-# }
+aws = {
+  access_key_id: ENV["REFILE_S3_ACCESS_KEY_ID"],
+  secret_access_key: ENV["REFILE_S3_SECRET_ACCESS_KEY"],
+  region: ENV["REFILE_S3_REGION"],
+  bucket: ENV["REFILE_S3_BUCKET"]
+}
 
-# Refile.cache = Refile::S3.new(prefix: "cache", **aws)
-# Refile.store = Refile::S3.new(prefix: "store", **aws)
+Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+Refile.store = Refile::S3.new(prefix: "store", **aws)
