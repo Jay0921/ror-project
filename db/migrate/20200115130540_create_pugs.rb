50a9a9a1765cb1348796909a6803e0c6eda56845
Jay <purporz@gmail.com>
2020-04-17 21:43:00 +0800

    Implement UUID
    - Enable UUID
    - Replace existing model ID to UUID
    - Set UUID as primary key

--- db/migrate/20200115130540_create_pugs.rb
+++ db/migrate/20200115130540_create_pugs.rb
@@ -1,6 +1,6 @@
 class CreatePugs < ActiveRecord::Migration[6.0]
   def change
-    create_table :pugs do |t|
+    create_table :pugs, id: :uuid  do |t|
       t.string :name, null: false
       t.integer :age, default: 0
 
